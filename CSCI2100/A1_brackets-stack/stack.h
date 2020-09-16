d78f771de13ba470e9915fff96775dc3e52648e9
thomplth <29663345+thomplth@users.noreply.github.com>
2020-09-16 23:25:16 +0800

    Removed duplicates.

--- CSCI2100/A1_brackets-stack/stack.h
+++
@@ -1,8 +0,0 @@
-typedef struct stackCDT *stackADT;
-
-typedef char stackElementT;
-
-stackADT EmptyStack(void);
-void Push(stackADT stack, stackElementT element); stackElementT Pop(stackADT stack);
-int StackDepth(stackADT stack);
-int StackIsEmpty(stackADT stack);
--- ENGG1110/vending-machine/README.md
+++
@@ -1,1 +0,0 @@
-This is the final course project of ENGG1110 Problem Solving By Programming.

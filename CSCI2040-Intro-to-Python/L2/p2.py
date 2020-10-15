09a57d0a03f02b0b6ff14cd35c1edc245011d368
thomplth <29663345+thomplth@users.noreply.github.com>
2020-10-15 22:45:51 +0800

    Modified directory names.

---
+++ CSCI2040-Intro-to-Python/L2/p2.py
@@ -0,0 +1,10 @@
+while True:
+    n = int(input("Enter an integer: "))
+    if n == -1:
+        break
+    str = input("Enter a string: ")
+
+    for i in range(1, n + 1):
+        print(" " * (n - i) * len(str) + str * i)
+
+print("Program ends.")
---
+++ CSCI2040-Intro-to-Python/L2/p4.py
@@ -0,0 +1,25 @@
+import getpass
+
+p = getpass.getpass(prompt="Player 1, write down your number secretly: ")
+
+while True:
+    try:
+        n = int(p)
+        if n < -100 or n > 100:
+            raise ValueError
+        break
+    except ValueError:
+        p = getpass.getpass(prompt="Player 1, invalid input, write down your number secretly: ")
+
+for count in range(1, 7):
+    guess = int(input("Player 2, input your guess: "))
+    if guess == n:
+        print("You are right after trying for {} times.Program ends.".format(count))
+        break
+    elif count == 6:
+        print("You have tried 6 times and it is still wrong!The answer is {} and program ends.".format(n))
+        break
+    elif guess < n:
+        print("Your guess is too low!")
+    elif guess > n:
+        print("Your guess is too high!")

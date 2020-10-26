097495b274f3ca810b9b23276476e266c96cd2f8
thomplth <29663345+thomplth@users.noreply.github.com>
2020-10-26 19:23:17 +0800

    Added p2.

--- CSCI2040-Intro-to-Python/L3/p2.py
+++ CSCI2040-Intro-to-Python/L3/p2.py
@@ -2,11 +2,12 @@ def quicksort(a):
     if not a:
         return []
 
-    p = (0 + len(a) - 1) // 2
-    n = a.pop(p)
+    p = a.pop((0 + len(a) - 1) // 2)
 
-    low = quicksort([x for x in a if x < n])
-    high = quicksort([y for y in a if y >= n])
-    result = low + [n] + high
+    low = quicksort([x for x in a if x < p])
+    high = quicksort([y for y in a if y >= p])
+    result = low + [p] + high
 
     return result
+
+# quicksort = lambda a: quicksort([x for x in a[1:] if x <= a[0]]) + [a[0]] + quicksort([x for x in a if x > a[0]]) if a else []

ddf2858c51d4aa8fedc1eda06ccab61d8cfe27b1
thomplth <29663345+thomplth@users.noreply.github.com>
2020-10-26 21:00:10 +0800

    Added p3.

--- CSCI2040-Intro-to-Python/L3/p3.py
+++ CSCI2040-Intro-to-Python/L3/p3.py
@@ -1,7 +1,46 @@
-def fibo(x1, x2):
-    if x2 > 1000:
-        return x2
-        
-    value = fibo(x2, x1 + x2)
-    
-    return value
+import pickle
+
+
+def load_data(file):
+    try:
+        f = open(file, 'rb')
+        result = pickle.load(f)
+        f.close()
+        return result
+    except Exception as e:
+        print(e)
+        print("File does not exist")
+
+
+def query_following(user_name):
+    user_dict = load_data("followers.pydata")
+    result = 0
+    for l in user_dict.values():
+        for i in l:
+            if i == user_name:
+                result += 1
+    return result
+
+
+def update():
+    try:
+        user_dict = load_data("followers.pydata")
+
+        user_dict["William Smith"].append("Lorna Carrico")
+        user_dict["Anne Smelcer"] = ["Christine Phillips", "Charles Mason", "Tim Lathrop"]
+
+        f = open('followers-updated.pydata', 'wb')
+        pickle.dump(user_dict, f)
+        f.close()
+
+    except Exception as e:
+        print(e)
+        print("File not stored")
+
+
+def get_num_of_followers():
+    update()
+    result = load_data("followers-updated.pydata")
+    for k in result.keys():
+        result[k] = len(result[k])
+    return result
